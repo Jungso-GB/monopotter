b913c14be3212420561e9d670301a5d6d4b2d1f4
sernoz <48032984+sernoz@users.noreply.github.com>
2023-05-20 10:45:23 +0200

    Slash Commands Core

--- Commands/start.js
+++ Commands/start.js
@@ -4,6 +4,9 @@ module.exports = {
 
     name: 'start',
     description: 'Start the Monop Otter!',
+    permission: "Aucune",
+    dm: true,
+    
     async run(bot, message){
 
         await message.reply('Starting Monop Otter...');
--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
+const loadSlashCommands = require('../Loader/loadSlashCommands');
 
 module.exports = async (bot) => {
+
+    await loadSlashCommands(bot)
 
     console.log(`${bot.user.tag} is ready`);
 
