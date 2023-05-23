7838d33591e3558e2ebd03d9584d45718256f898
sernoz <48032984+sernoz@users.noreply.github.com>
2023-05-23 20:41:58 +0200

    Switch to env token for deployement

--- main.js
+++ main.js
@@ -6,12 +6,11 @@ const bot = new Discord.Client({intents});
 const loadCommands = require('./Loader/loadCommands');
 const loadEvents = require('./Loader/loadEvents');
 
-const config = require('./token.js'); // Put your token in token.js
 bot.color = "#95A5A6" // Set bot color
 
 bot.commands = new Discord.Collection(); // Create collection of commands
 
-bot.login(config.token); // Login to Discord
+bot.login(process.env.BOT_TOKEN); // Login to Discord
 
 loadCommands(bot); // Load all commands in collection, to the bot
 loadEvents(bot); // Load all commands in collection, to the bot
