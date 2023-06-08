53236727cbacfa0544af4e84708830b502756154
sernoz <48032984+sernoz@users.noreply.github.com>
2023-06-08 09:09:59 +0200

    Help command fix

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -5,11 +5,11 @@ module.exports = async (bot, interaction) => {
     //Autocomplete slash command
     if(interaction.type === Discord.InteractionType.ApplicationCommandAutocomplete) {
         let entry = interaction.options.getFocused() // Take the field entry when the typing
-        command.log(interaction.commandName)
+        //command.log(interaction.commandName)
         if(interaction.commandName === "help") {
 
         //Choice in bot.comands then .filter
-        let choices = bot.comands.filter(cmd => cmd.name.includes(entry))
+        let choices = bot.commands.filter(cmd => cmd.name.includes(entry))
         //  Show                                If nothing is typed,                     
         await interaction.respond(entry === "" ? bot.commands.map(cmd => ({name: cmd.name, value: cmd.name})) : choices.map(choice => ({name: choice.name, value: choice.name})))
 
